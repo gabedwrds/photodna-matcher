c43e7da056951fa89e140ea1ac43da7609b256e3
Gabe Edwards <gabe.edwards@me.com>
2021-08-20 22:01:25 -0700

    Working

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+type File struct {
+	FileName       string
+	PhotoDNA       []byte
+	BestMatchIndex int
+	BestMatchScore int
+	lock           sync.Mutex
+}
+
+func main() {
+	log.Println("loading data")
+	hashCsv, err := os.Open("hashes.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer hashCsv.Close()
+
+	csvLines, err := csv.NewReader(hashCsv).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	files := make([]File, 0, 1000)
+
+	lineNum := 0
+	for _, line := range csvLines {
+		pdna, _ := base64.StdEncoding.DecodeString(line[1])
+		if len(pdna) != 144 {
+			panic("wrong hash length for file: " + line[0])
+		}
+		lineNum++
+		matchInfo := File{
+			FileName:       line[0],
+			PhotoDNA:       pdna,
+			BestMatchIndex: -1,
+			BestMatchScore: int(^uint(0) >> 1),
+		}
+		files = append(files, matchInfo)
+	}
+
+	// log.Println("hashes: ", len(hashes))
+	// log.Println("files: ", len(files))
+
+	var wg sync.WaitGroup
+
+	log.Println("starting work")
+
+	for index, _ := range files {
+		wg.Add(1)
+		go hashForFile(&wg, index, files)
+	}
+
+	wg.Wait()
+
+	log.Println("workers finished")
+
+	for i, file := range files {
+		fmt.Println(i, file.FileName, file.BestMatchScore, "from", file.BestMatchIndex, files[file.BestMatchIndex].FileName)
+	}
+}
+
+func hashForFile(wg *sync.WaitGroup, index int, files []File) {
+	defer wg.Done()
+
+	thisPdna := files[index].PhotoDNA
+
+	for i := index + 1; i < len(files); i++ {
+		distance := calcDistance(thisPdna, files[i].PhotoDNA)
+
+		if distance == 0 {
+			// log.Println("skipping identical images", files[index].FileName, files[i].FileName)
+			continue
+		}
+
+		if distance > files[index].BestMatchScore && distance > files[i].BestMatchScore {
+			// bad match, don't bother locking for updates
+			continue
+		}
+
+		files[index].lock.Lock()
+		if distance < files[index].BestMatchScore || (distance == files[index].BestMatchScore && i < files[index].BestMatchIndex) {
+			// log.Println("distance:", distance, "from", i)
+			files[index].BestMatchIndex = i
+			files[index].BestMatchScore = distance
+		}
+		files[index].lock.Unlock()
+
+		files[i].lock.Lock()
+		if distance < files[i].BestMatchScore || (distance == files[i].BestMatchScore && index < files[i].BestMatchIndex) {
+			// log.Println("updating from other side:", i, "is", distance, "from", index)
+			files[i].BestMatchIndex = index
+			files[i].BestMatchScore = distance
+		}
+		files[i].lock.Unlock()
+	}
+
+	if index%1000 == 0 {
+		log.Println("worker finished:", index)
+	}
+}
+
+func calcDistance(h1, h2 []byte) (distance int) {
+	distance = 0
+	if len(h1) != 144 {
+		panic("h1 wrong length")
+	}
+	if len(h2) != 144 {
+		panic("h2 wrong length")
+	}
+	for i := 0; i < 144; i++ {
+		distance += (int(h1[i]) - int(h2[i])) * (int(h1[i]) - int(h2[i]))
+	}
+	return
+}
---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/base64"
+	"testing"
+)
+
+func TestDistance(t *testing.T) {
+	h1, _ := base64.StdEncoding.DecodeString("EX0BcwIkBf8MBQf/CgUA/CABAe1lHwhkCdYDFhY7IRggKTUkGRUlGi8MIBrgFQcZDLwHDCMsHQw+M0AWRjA6GChJLB7bGwUUAswHCD0dEw4eSh0rMSUPJxguCyngBQgMAtUSBhcTHAsIGR0UCBUjDREJJwfdABgJAXCLAAUL5AAEB+MABQjsAA8B8QBkAJMA")
+	h2, _ := base64.StdEncoding.DecodeString("AngArQQbAf8XEAL/GwgC/xcIA+ZqAQGXBeYEEQ00DRomIisgNg0qKzAWDRvOBAMcBd4NARMgHAFDIUgVMTI4DScrFALeEQoGBNIHAhQYDAQvIxghLyUQHCgpCgbZFQoEBMoUABMSHgMZGC8GIBwiDikjFQ/SCx8CAGWaAAkH3wENC9gBER7rBRkI3QRmAYsA")
+
+	distance := calcDistance(h1, h2)
+	if distance != 28315 {
+		t.Error("wrong distance", distance)
+	}
+}
